2f09f90587622030c8bdda3d21dfba8317c16e7b
RafagaBlanca <jorgedevthings3004@gmail.com>
2024-09-10 16:11:43 -0600

    Profiling & KMeans

--- scripts/lab1.py
+++ scripts/lab1.py
@@ -1,5 +1,23 @@
-import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-df = pd.read_csv('./data/bezdekIris.csv')
-print(df)
+
+iris = load_iris()
+iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names) # type: ignore
+iris_df['target'] = iris.target #type: ignore
+iris_df.head()
+
+kmeans = KMeans(n_clusters=3, init = 'k-means++', max_iter=300, random_state=0)
+kmeans.fit(iris.data)
+print(kmeans.labels_)
+
+iris_df['cluster'] = kmeans.labels_
+iris_df.groupby(['target', 'cluster']).agg({'sepal length (cm)':'count'})
+
+
+
+
+
